fa1922bdf63a8bc5915f9dc7b0bd575686e8e793
logic.bai <logicbai@foxmail.com>
2024-03-26 16:48:10 +0800

    1. Added the function of simulating I2C master read I2C slave

--- src/i2c_slave_tb.v
+++ src/i2c_slave_tb.v
@@ -46,21 +46,26 @@ module i2c_slave_tb();
     initial mod_clk = 1;
     always#10 mod_clk = ~mod_clk;
 
-    parameter DATA_TO_SEND = 4;
+    parameter DATA_TO_SEND = 8;
+    parameter DATA_TO_READ = 8;
 
     integer i;
     integer j;
-    reg [(8*DATA_TO_SEND-1):0] byte_data_array = {(8'h45 << 1), 8'h4, 8'h1, 8'h23};
+    reg [(8*DATA_TO_SEND-1):0] byte_write_data_array = {(8'h45 << 1), 8'h4, 8'h1, 8'h08, 8'h23, 8'h45, 8'h01, 8'h02};
+    reg [(8*DATA_TO_READ-1):0] byte_read_data_array = {(8'h45 << 1), 8'h0, ((8'h45 << 1) + 8'h1), 8'h5, 8'h1, 8'h08, 8'h23, 8'h45};
     reg [7:0] byte_data;
 
     initial begin
+        i2c_reg_data_ready = 0;
+        /******************** code segement start for i2c write *************************/
         i2c_clk = 1;
         i2c_sda = 1;
         #100 i2c_clk = 1;
         i2c_sda = 0;
 
+        // i2c_slave write process
         for(j = (DATA_TO_SEND - 1); j >= 0; j = j - 1) begin
-            byte_data = byte_data_array[((j + 1)*8-1)-:8];
+            byte_data = byte_write_data_array[((j + 1)*8-1)-:8];
             for(i = 7; i >= 0; i = i - 1) begin
             // send one bit
             #100 i2c_clk = 0;
@@ -78,6 +83,90 @@ module i2c_slave_tb();
         #100 i2c_sda = 0;
         #100 i2c_clk = 1;
         #100 i2c_sda = 1;
+        /******************** code segement end for i2c write *************************/
+
+        // i2c start condition
+        #100 i2c_clk = 1;
+        i2c_sda = 1;
+        #100 i2c_clk = 1;
+        i2c_sda = 0;
+
+        // i2c master set register address
+        for(j = (DATA_TO_READ); j >= (DATA_TO_READ - 1); j = j - 1) begin
+            byte_data = byte_read_data_array[(j*8-1)-:8];
+            for(i = 7; i >= 0; i = i - 1) begin
+            // send one bit
+            #100 i2c_clk = 0;
+            #100 i2c_sda = byte_data[i];
+            #100 i2c_clk = 1;
+            end
+            // send one ack
+            #100 i2c_clk = 0;
+            #100 i2c_sda = 1;
+            #100 i2c_clk = 1;
+        end
+
+        // i2c stop condition
+        #100 i2c_clk = 0;
+        #100 i2c_sda = 0;
+        #100 i2c_clk = 1;
+        #100 i2c_sda = 1;
+
+        // i2c start condition
+        #100 i2c_clk = 1;
+        i2c_sda = 1;
+        #100 i2c_clk = 1;
+        i2c_sda = 0;
+
+        // send device address
+        byte_data = byte_read_data_array[((DATA_TO_READ - 2)*8-1)-:8];
+        for(i = 7; i >= 0; i = i - 1) begin
+        // send one bit
+        #100 i2c_clk = 0;
+        #100 i2c_sda = byte_data[i];
+        #100 i2c_clk = 1;
+        end
+        // send one ack
+        #100 i2c_clk = 0;
+        #100 i2c_sda = 1;
+        #100 i2c_clk = 1;
+
+        for(j = (DATA_TO_READ - 3); j >= 2; j = j - 1) begin
+            i2c_data_in = byte_read_data_array[(j*8-1)-:8];
+            #20 i2c_reg_data_ready = 1;
+            #20 i2c_reg_data_ready = 0;
+            for(i = 7; i >= 0; i = i - 1) begin
+            // read one bit
+            #100 i2c_clk = 0;
+            #100 i2c_sda = 1;
+            #100 i2c_clk = 1;
+            end
+            // send one ack
+            #100 i2c_clk = 0;
+            #100 i2c_sda = 0;
+            #100 i2c_clk = 1;
+        end
+
+        i2c_data_in = byte_read_data_array[7:0];
+        #20 i2c_reg_data_ready = 1;
+        #20 i2c_reg_data_ready = 0;
+        // read last byte
+        for(i = 7; i >= 0; i = i - 1) begin
+        #100 i2c_clk = 0;
+        #100 i2c_sda = 1;
+        #100 i2c_clk = 1;
+        end
+        // send NAK in the end byte
+        #100 i2c_clk = 0;
+        #100 i2c_sda = 1;
+        #100 i2c_clk = 1;
+
+        // i2c stop condition
+        #100 i2c_clk = 0;
+        #100 i2c_sda = 0;
+        #100 i2c_clk = 1;
+        #100 i2c_sda = 1;
+
         #100
         $stop;
     end
